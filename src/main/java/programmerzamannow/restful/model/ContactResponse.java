ef8b6fec65dc2c3c2c10a1bda7c634d0c52a624c
Ardiansyah Sulistyo <ardiansyahsulistyo@gmail.com>
2024-07-15 16:10:23 +0700

    Create Contact API

---
+++ src/main/java/programmerzamannow/restful/model/ContactResponse.java
@@ -0,0 +1,24 @@
+package programmerzamannow.restful.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ContactResponse {
+
+    private String id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String phone;
+
+}
---
+++ src/main/java/programmerzamannow/restful/repository/ContactRepository.java
@@ -0,0 +1,10 @@
+package programmerzamannow.restful.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import programmerzamannow.restful.entity.Contact;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, String> {
+
+}
