7499d0e8165e353a08ee29134919e5f571e06f1b
Ardiansyah Sulistyo <ardiansyahsulistyo@gmail.com>
2024-07-16 11:08:54 +0700

    Create Address API

---
+++ src/test/java/programmerzamannow/restful/controller/AddressControllerTest.java
@@ -0,0 +1,123 @@
+package programmerzamannow.restful.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder.*;
+import programmerzamannow.restful.entity.Contact;
+import programmerzamannow.restful.entity.User;
+import programmerzamannow.restful.model.AddressResponse;
+import programmerzamannow.restful.model.CreateAddressRequest;
+import programmerzamannow.restful.model.WebResponse;
+import programmerzamannow.restful.repository.AddressRepository;
+import programmerzamannow.restful.repository.ContactRepository;
+import programmerzamannow.restful.repository.UserRepository;
+import programmerzamannow.restful.security.BCrypt;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AddressControllerTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        addressRepository.deleteAll();
+        contactRepository.deleteAll();
+        userRepository.deleteAll();
+
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword(BCrypt.hashpw("test", BCrypt.gensalt()));
+        user.setName("Test");
+        user.setToken("test");
+        user.setTokenExpiredAt(System.currentTimeMillis() + 100000000L);
+        userRepository.save(user);
+
+        Contact contact = new Contact();
+        contact.setId("test");
+        contact.setUser(user);
+        contact.setFirstName("Ardiansyah");
+        contact.setLastName("Sulistyo");
+        contact.setEmail("ardiansyah@example.com");
+        contact.setPhone("088465762563");
+        contactRepository.save(contact);
+    }
+
+    @Test
+    void testCreateAddressBadRequest() throws Exception {
+        CreateAddressRequest request = new CreateAddressRequest();
+        request.setCountry("");
+
+        mockMvc.perform(
+                post("/api/contacts/test/addresses")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                        .header("X-API-TOKEN", "test")
+        ).andExpectAll(
+                status().isBadRequest()
+        ).andDo(result -> {
+            WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+            });
+            assertNotNull(response.getErrors());
+        });
+    }
+
+    @Test
+    void testCreateAddressSuccess() throws Exception {
+        CreateAddressRequest request = new CreateAddressRequest();
+        request.setStreet("Jalan");
+        request.setProvince("Provinsi");
+        request.setCity("Kota");
+        request.setCountry("Indonesia");
+        request.setPostalCode("12312");
+
+        mockMvc.perform(
+                post("/api/contacts/test/addresses")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                        .header("X-API-TOKEN", "test")
+        ).andExpectAll(
+                status().isOk()
+        ).andDo(result -> {
+            WebResponse<AddressResponse> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+            });
+            assertNull(response.getErrors());
+            assertEquals(request.getStreet(), response.getData().getStreet());
+            assertEquals(request.getProvince(), response.getData().getProvince());
+            assertEquals(request.getCity(), response.getData().getCity());
+            assertEquals(request.getCountry(), response.getData().getCountry());
+            assertEquals(request.getPostalCode(), response.getData().getPostalCode());
+            assertTrue(addressRepository.existsById(response.getData().getId()));
+        });
+    }
+}
